de3af6556f6fbc622f8f576a561ee146d05588a0
wxh <1297627104@qq.com>
2020-05-16 13:59:12 +0800

    登录的第二种方法提交

--- admin/js/login.js
+++ admin/js/login.js
@@ -1,37 +1,35 @@
 // 1.书写入口函数
 $(function () {
-    // 2.给登录按钮注册事件
-    $('input_sub').on('click', function (e) {
-        // 3.阻止form的默认提交行为
+    // 2.给form表单注册submit事件
+    $('.login_form').on('submit', function (e) {
+        // 3.阻止默认提交方式
         e.preventDefault();
-        // 4.获取登录的数据
-        var username = $('.input_txt').val();
-        var password = $('.input_pass').val();
-        // 5.判断用户名和密码是否为空
-        if ($.trim(username) == '' && $trim(password) == '') {
-            alert('用户名和密码不能为空，请重新输入···');
-            return;  // 阻止代码继续向下执行
-        };
-        // 6.发送ajax请求
+        // 4.发送ajax亲请求
         $.ajax({
             type: 'post',
             url: 'http://localhost:8080/api/v1/admin/user/login',
-            data: {
-                username: username,
-                password: password,
+            data: $(this).serialize(),
+            beforeSend: function () {
+                // 5.发送请求之前判断用户输入的用户名或者密码是否为空\
+                var flag = false;
+                $('.login_form input[name]').each(function (index, ele) {
+                    if ($.trim($(ele).val()) == '') {
+                        flag = true;
+                    };
+                });
+                if (flag) {
+                    alert('输入的用户名或密码不能为空');
+                    return false;  // 阻止请求的发送
+                }
             },
             success: function (res) {
-                // console.log(res);
-                // console.log(typeof res);
-                
                 if (res.code == 200) {
                     alert('登录成功');
-                    // 7.跳转到主页面
-                    window.location.href = './index.html'
+                    window.location.href = './index.html';
                 }else{
-                    alert(res.msg) // 提示用户错误
+                    alert(res.msg);
                 };
             },
         });
-    })
+    });
 })
