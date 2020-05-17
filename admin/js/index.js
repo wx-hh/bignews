0a7840e179dbb22270f7f3bc1d3fd91c6ed67427
wxh <1297627104@qq.com>
2020-05-17 16:42:46 +0800

    首页左侧特效点击高亮效果完成

--- admin/js/index.js
+++ admin/js/index.js
@@ -4,7 +4,7 @@ $(function () {
     $.ajax({
         type: 'get',
         // url: 'http://localhost:8080/api/v1/admin/user/info',
-        url:BigNew.user_info,
+        url: BigNew.user_info,
         // 携带token 口令访问服务器
         headers: {
             'Authorization': localStorage.getItem('token'),
@@ -33,5 +33,28 @@ $(function () {
         localStorage.removeItem('token');
         // 跳转回登录页面
         location.href = './login.html';
+    });
+
+
+    // 获取左侧的几个a标签并注册事件
+    $('.menu .level01').on('click', function () {
+        $(this).addClass('active').siblings().removeClass('active');
+        // 当点击第二个时 要让后边的三角进行旋转
+        if ($(this).index() == 1) {  // 用索引来判断获取的是第几个div
+            // 当点击这个div时要让下边的ul显示 就是切换隐藏或者显示
+            $('.menu .level02').slideToggle();
+            // 同时还需要实现三角的旋转
+            $(this).find('b').toggleClass('rotate0')
+
+            // 当ul显示的时候就默认让里边的第一个li标签有一个高亮的效果
+            $('.menu .level02 li:eq(0)').addClass('active')
+        };
+
+        
+    });
+
+    // 当点击ul里面的li标签时 让点击的li标签有一个高亮度额显示
+    $('.menu .level02 li').on('click', function () {
+        $(this).addClass('active').siblings().removeClass('active');
     })
 })
